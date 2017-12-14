30c116f3ff3bd57fc0a14778121d7fdd8cbe51fb
Thivya <thivythiya@gmail.com>
2017-12-14 16:41:42 +0530

    Initial setup and project architecture.

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+	entry:'./src/index.js',
+
+	output:{
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+
+	module:{
+		rules:[
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				use:{
+					loader:'babel-loader',
+					options: {
+					 	presets: ['env','react']
+					}
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [ 'style-loader', 'css-loader','sass-loader']
+			}
+		]
+	},
+	resolve:{
+		extensions:['.js','.jsx','.css']
+	},
+
+	devServer:{
+		contentBase: path.join(__dirname, 'dist')
+	}
+
+};
+
+module.exports = config;
