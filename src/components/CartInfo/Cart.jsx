f8fa4add6633932268c66253971233aea0654233
Thivya <thivythiya@gmail.com>
2018-01-01 15:31:06 +0530

    According to the quantity count, calculate the total of each selected product

--- src/components/CartInfo/Cart.jsx
+++ src/components/CartInfo/Cart.jsx
@@ -1,46 +1,81 @@
 import React from 'react';
 import './cartStyle';
-class Cart extends React.Component{
-	constructor(){
-		super()
+
+let quantity = [1,2,3,4,5,6,7,8,9,10]
+
+class CartDetails extends React.Component{
+	constructor(props){
+		super(props);
+
 		this.state = {
-			qty: 1
+			itmQty: 1
 		}
+		this.itemQuantity = this.itemQuantity.bind(this);
+
+
 	}
-	quantity(event){
-	let {qty} = this.state;
-		this.setState({qty: event.target.value})
-		console.log("qty", qty);
+
+	itemQuantity(e){
+		let {itmQty} = this.state;
+		this.setState({itmQty: e.target.value});
+		console.log('quantity',itmQty)
 	}
+
 	render(){
-		let {cartItems} = this.props;
-		let {qty} = this.state;
-		console.log('quantity',this.state.qty);
+		
+		let {item,keyVal} = this.props;
+		let {itmQty} = this.state;
+		
+		console.log("item", item);
+
 		return (
+				<div className="quality-box" key={keyVal}>
+					<div className="ordered-image">
+						<img src={item.image} alt="image not available"/>
+					</div>
+					<div className="ordered-details">
+						<div>Order Id:<span>{item.id}</span></div>
+						<div>Ordered Item: <span>{item.name}</span></div>
+						<div>Qty:
+							
+								<select onChange={this.itemQuantity}>
+									{
+										quantity.map((qty,i) => {
+											return <option value={qty} key={i}>{qty}</option>
+										})
+									}
+								</select>
+							
+						</div>
+						<div>Price: <span>₹{item.price}</span></div>
+					</div>
+					<div className="ordered-total"><p>Total: <span>₹{ itmQty * item.price}</span></p></div>
+				</div>
+
+			)
+
+	}
+}
+
+let Cart = (props) => {
+	
+	console.log("props", props);
+
+
+
+	return (
 		<div className="cart-items">
 			<div>
 				<h2>Cart</h2>
-				<div>
-					{
-					cartItems.map((item, i) =>
-						<div className="quality-box" key={i}>
-							<div className="ordered-image">
-								<img src={item.image} alt="image not available"/>
-							</div>
-							<div className="ordered-details">
-								<div>Order Id:<span>{item.id}</span></div>
-								<div>Ordered Item: <span>{item.name}</span></div>
-								<div>Qty: <span><input type="number" onChange={this.quantity.bind(this)}/></span></div>
-								<div>Price: <span>₹{item.price}</span></div>
-							</div>
-							<div className="ordered-total"><p>Total: <span>₹{qty * item.price}</span></p></div>
-						</div>
-					)
-					}
-				</div>
+				{
+					props.cartItems.map((item,i) => {
+						return <CartDetails item={item} key={i}/>
+					})
+				}
 			</div>
-		</div>	
-		)
-	}
+		</div>
+	)
 }
+
+
 export default Cart;
