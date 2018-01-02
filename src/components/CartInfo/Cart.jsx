d77eaf5421def7c8a6cb69e81062490c35a630f0
Thivya <thivythiya@gmail.com>
2018-01-02 19:02:16 +0530

    Change the cart page with table

--- src/components/CartInfo/Cart.jsx
+++ src/components/CartInfo/Cart.jsx
@@ -11,67 +11,69 @@ class CartDetails extends React.Component{
 			itmQty: 1
 		}
 		this.itemQuantity = this.itemQuantity.bind(this);
-
-
 	}
 
 	itemQuantity(e){
 		let {itmQty} = this.state;
 		this.setState({itmQty: e.target.value});
-		console.log('quantity',itmQty)
 	}
 
 	render(){
 		
 		let {item,keyVal} = this.props;
-		let {itmQty} = this.state;
-		
+		let {itmQty} = this.state;	
 		console.log("item", item);
-
 		return (
-				<div className="quality-box" key={keyVal}>
-					<div className="ordered-image">
-						<img src={item.image} alt="image not available"/>
-					</div>
-					<div className="ordered-details">
-						<div>Order Id:<span>{item.id}</span></div>
-						<div>Ordered Item: <span>{item.name}</span></div>
-						<div>Qty:
-							
-								<select onChange={this.itemQuantity}>
-									{
-										quantity.map((qty,i) => {
-											return <option value={qty} key={i}>{qty}</option>
-										})
-									}
-								</select>
-							
-						</div>
-						<div>Price: <span>₹{item.price}</span></div>
-					</div>
-					<div className="ordered-total"><p>Total: <span>₹{ itmQty * item.price}</span></p></div>
-				</div>
-
+				<tr className='table-col'>
+					<td>{item.index}</td>
+					<td>{item.id}</td>
+					<td className='table-col-name'>{item.name}</td>
+					<td>
+						<select onChange={this.itemQuantity}>
+							{
+								quantity.map((qty,i) => {
+									return <option value={qty} key={i}>{qty}</option>
+								})
+							}
+						</select>
+					</td>
+					<td>₹{itmQty*item.price}</td>
+				</tr>
 			)
 
 	}
 }
 
 let Cart = (props) => {
-	
 	console.log("props", props);
-
-
-
 	return (
 		<div className="cart-items">
 			<div>
 				<h2>Cart</h2>
-				{
-					props.cartItems.map((item,i) => {
-						return <CartDetails item={item} key={i}/>
-					})
-				}
+				<div className='table-container'>
+					<table>
+						<thead>
+							<tr>
+								<th>Sl.No</th>
+								<th>Product Id</th>
+								<th>Product Name</th>
+								<th>Qty</th>
+								<th>Price</th>
+							</tr>
+						</thead>
+						<tbody>
+							{
+							props.cartItems.map((item,i) => {
+								return <CartDetails item={item} key={i}/>
+							})
+							}
+							<tr>
+								<td colSpan="4">Total</td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	)
